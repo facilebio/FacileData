08831a660c60e9ab32c914b67caa9f4a7e64e30a
lianogls <lianogls@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-05-27 21:37:48 +0000

    changed to plotting log2(counts) instead of log2(cpm) for clarity
    git-svn-id: http://resscm.gene.com/bioinfo/projects/FacileData/Rpkgs/FacileRepo/trunk@190718 ea8427e4-251f-0410-985a-fb3ea31d2ee2

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,14 @@
-##' Like dcast but the first row becomes rownames of a matrix object
+##' Cast a data.frame into a matrix.
+##'
+##' This is a simple wrapper to \code{\link[reshape2]{dcast}} which converts
+##' the \code{data.frame} that call would create into a matrix which uses the
+##' first column as the rownames.
+##'
 ##' @export
+##' @param data a \code{data.frame} to whip into a matrix
+##' @param formula the pivot formula
+##' @param fun.aggregate the aggregate function
+##' @param ... args past down to \code{\link[reshape2]{dcast}}.
 mcast <- function(data, formula, fun.aggregate=NULL, ...) {
   d <- dcast(data, formula, fun.aggregate, ...)
   set_rownames(as.matrix(d[, -1L, drop=FALSE]), d[[1L]])
