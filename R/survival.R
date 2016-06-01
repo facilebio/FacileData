784a8b93166824b2618f2cf19f4e23f54aa8c8a5
lianogls <lianogls@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-06-01 23:51:02 +0000

    moving survival stuff into FacileRepo
    git-svn-id: http://resscm.gene.com/bioinfo/projects/FacileData/Rpkgs/FacileRepo/trunk@190984 ea8427e4-251f-0410-985a-fb3ea31d2ee2

---
+++ R/survival.R
@@ -0,0 +1,17 @@
+##' Convert +/- encoding of time to event to time + event.
+##'
+##' Time to event (and event labels) are encoded as a single real value in the
+##' clinical data. The absoulte value of the value is the time to the event, and
+##' a negative value indicates "event" and a positive value is right censored.
+##'
+##' @export
+##' @param x the time to event
+##' @return two column data.frame
+parse_time_to_event <- function(x, suffix=NULL) {
+  x <- as.numeric(x)
+  out <- data.frame(tte=abs(x), event=as.integer(x < 0))
+  if (is.character(suffix)) {
+    names(out) <- paste0(names(out), '_', sub('^[^a-zA-Z]', '', suffix))
+  }
+  out
+}
